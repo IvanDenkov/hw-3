b87c4447b84f5f124a3461093d5aad078fbde549
IvanDenkov <denkov_v@ukr.net>
2021-10-15 19:15:51 +0300

    hw-3

---
+++ js/script.js
@@ -0,0 +1,11 @@
+let admin;
+const name = "Джон";
+admin = name;
+alert(admin);
+const ourPlanetName = "Земля";
+const currentUserName = "Владимир";
+const num1 = 10;
+const num2 = "10";
+const result = num1 + num2;
+
+console.log(result,typeof result);
